4c7257c63353a0e1a025624bc45288cf53f8778d
Victoria Black <102681741+victoriaskylanp@users.noreply.github.com>
2023-04-03 10:48:03 -0600

    updating error page

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
                       <Route index element={<Home/>}/>
                       <Route path='/gallery' element={<Gallery/>}/>
                       <Route path='/contact' element={<Contact/>}/>
+                      <Route path='*' element={<Home/>}/>
                 </Route>
             </Routes>
           <Footer />
